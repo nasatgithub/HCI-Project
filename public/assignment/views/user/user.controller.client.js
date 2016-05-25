13e857c2df0be0cb120bdac015644de10040cdef
Jose Annunziato <jannunzi@gmail.com>
2016-05-24 20:28:29 -0400

    retrieve user by id bind instance to view for rendering

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -1,22 +1,35 @@
 (function(){
     angular
         .module("WebAppMaker")
-        .controller("LoginController", LoginController);
+        .controller("LoginController", LoginController)
+        .controller("ProfileController", ProfileController);
 
-    function LoginController() {
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    function ProfileController($routeParams) {
         var vm = this;
 
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ];
+        var id = $routeParams.id;
+
+        for(var i in users) {
+            if(users[i]._id === id) {
+                vm.user = users[i];
+            }
+        }
+    }
+
+    function LoginController($location) {
+        var vm = this;
 
         vm.login = function(username, password) {
             for(var i in users) {
                 if(users[i].username === username && users[i].password === password) {
-                    console.log("YAY");
+                    $location.url("/profile/"+users[i]._id);
                 } else {
                     vm.error = "User not found";
                 }
