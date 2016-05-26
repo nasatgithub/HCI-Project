ad43063159cfa0fda1eabea816c84df203060297
Jose Annunziato <jannunzi@gmail.com>
2016-05-26 18:49:47 -0400

    added website service

---
+++ public/assignment/views/website/website-list.controller.client.js
@@ -0,0 +1,14 @@
+(function(){
+    angular
+        .module("WebAppMaker")
+        .controller("WebsiteListController", WebsiteListController);
+
+    function WebsiteListController($routeParams, WebsiteService) {
+        var vm = this;
+
+        function init() {
+            vm.websites = WebsiteService.findWebsitesForUserId($routeParams.userId);
+        }
+        init();
+    }
+})();
