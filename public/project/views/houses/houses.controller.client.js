f6356f31d2411d2be8f3c3ac7f555414e8067858
skillet urs <skillet urs>
2016-12-06 22:30:56 -0500

    adding panels for houses

--- public/project/views/houses/houses.controller.client.js
+++ public/project/views/houses/houses.controller.client.js
@@ -10,16 +10,35 @@
         var vm = this;
         //vm.userId = $routeParams.userId;
         vm.housesDiv= 0;
+        vm.sugDiv=0;
         vm.housesList=housesList;
         function housesList(){
-            if(vm.room=="1b"){
-                vm.housesDiv = 1;}
-            else if (vm.room=="3b"){
-                vm.housesDiv = 3;}
-            else if (vm.room=="2b"){
-                vm.housesDiv = 2;}
-            
-
+            vm.sugDiv=1;
+            if(vm.room != null || vm.price == null ) {
+                if (vm.room == "1b") {
+                    vm.housesDiv = 1;
+                }
+                else if (vm.room == "3b") {
+                    vm.housesDiv = 3;
+                }
+                else if (vm.room == "2b") {
+                    vm.housesDiv = 2;
+                }
+            }
+            if (vm.price != null || vm.room==null){
+                if (vm.price == "all") {
+                    vm.housesDiv = 1;
+                }
+                else if (vm.price == "hun") {
+                    vm.housesDiv = 3;
+                }
+                else if (vm.price == "tho") {
+                    vm.housesDiv = 2;
+                }
+                else if (vm.price == "mil") {
+                    vm.housesDiv = 2;
+                }
+            }
         }
 
     }
