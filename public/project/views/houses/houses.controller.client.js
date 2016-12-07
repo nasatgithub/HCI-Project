d590a071c22a710d01e3bfbad8a8afd12c424cd2
skillet urs <skillet urs>
2016-12-06 23:08:09 -0500

    made more search results

--- public/project/views/houses/houses.controller.client.js
+++ public/project/views/houses/houses.controller.client.js
@@ -15,29 +15,33 @@
         vm.uName = $routeParams.uName;
         function housesList(){
             vm.sugDiv=1;
+            console.log(vm.room);
             if(vm.room != null || vm.price == null ) {
                 if (vm.room == "1b") {
                     vm.housesDiv = 1;
                 }
                 else if (vm.room == "3b") {
+                    vm.sugDiv=2;
                     vm.housesDiv = 3;
                 }
                 else if (vm.room == "2b") {
                     vm.housesDiv = 2;
                 }
             }
-            if (vm.price != null || vm.room==null){
+            if (vm.price != null && vm.room==null){
                 if (vm.price == "all") {
                     vm.housesDiv = 1;
                 }
                 else if (vm.price == "hun") {
-                    vm.housesDiv = 3;
+                    vm.housesDiv = 4;
                 }
                 else if (vm.price == "tho") {
-                    vm.housesDiv = 2;
+                    vm.sugDiv=2;
+                    vm.housesDiv = 5;
                 }
                 else if (vm.price == "mil") {
-                    vm.housesDiv = 2;
+                    vm.sugDiv=2;
+                    vm.housesDiv = 6;
                 }
             }
         }
